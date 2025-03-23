593c8e4a013312f3296a2d2ccd8b4aa46eaff57b
Muhammad Atiqi Hazizan <aqi852000@gmail.com>
2025-03-23 09:37:30 +0800

    fix: update card component width and rebuild assets

--- src/ui/card.jsx
+++ src/ui/card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ title, children, contentClass = "" }) => {
   return (
-    <div className="flexCard bg-gray-900 text-white rounded-lg shadow-xl p-6 w-[490px]">
+    <div className="flexCard bg-gray-900 text-white rounded-lg shadow-xl p-6 w-full">
       {/* Header */}
       <div className="header text-center mb-6">
         <p className="title text-4xl w-full font-semibold text-gray-300">{title}</p>
